8eb8e245d5bdad6ea1c7ec8476c2fa628339e9c7
Max Strange <maxfieldstrange@gmail.com>
2026-01-20 10:03:17 -0800

    WIP

---
+++ framework/ardk/services/artie-rpc-broker/src/service.py
@@ -0,0 +1,29 @@
+"""
+This module contains the code for a `Service` datastructure.
+"""
+from artie_util import artie_logging as alog
+
+class Service:
+    """
+    This class represents a service that can be registered
+    with the Artie RPC Broker.
+
+    This is meant to be an immutable, hashable object.
+    """
+    def __init__(self, fully_qualified_name: str, host: str, port: int):
+        self.fully_qualified_name = fully_qualified_name
+        self.simple_name = fully_qualified_name.split(":")[0]
+        self.interface_names = fully_qualified_name.split(":")[1:]
+        self.host = host
+        self.port = port
+
+    def __eq__(self, value):
+        return (
+            isinstance(value, Service) and
+            self.fully_qualified_name == value.fully_qualified_name and
+            self.host == value.host and
+            self.port == value.port
+        )
+
+    def __hash__(self):
+        return hash((self.fully_qualified_name, self.host, self.port))
