57e73f9ddbbcac8b03fc80e793c0c1c54bded8cb
Max Strange <maxfieldstrange@gmail.com>
2026-02-01 18:23:32 -0800

    WIP

--- artie-common/drivers/mouth-and-eyebrows/src-eyebrows/ebcommon.py
+++ artie-common/drivers/mouth-and-eyebrows/src-eyebrows/ebcommon.py
@@ -6,18 +6,18 @@ from artie_util import boardconfig_controller as board
 
 # I2C address for each eyebrow MCU
 MCU_ADDRESS_MAP = {
-    "left": board.I2C_ADDRESS_EYEBROWS_MCU_LEFT,
-    "right": board.I2C_ADDRESS_EYEBROWS_MCU_RIGHT,
+    "eyebrow-left": board.I2C_ADDRESS_EYEBROWS_MCU_LEFT,
+    "eyebrow-right": board.I2C_ADDRESS_EYEBROWS_MCU_RIGHT,
 }
 
 def get_address(side: str):
     """
     Return the address corresponding to the given `side`.
-    Raise a ValueError if 'side' is not one of 'left' or 'right'.
+    Raise a ValueError if 'side' is not one of 'eyebrow-left' or 'eyebrow-right'.
     """
     address = MCU_ADDRESS_MAP.get(side, None)
     if address is None:
-        errmsg = f"Given side: '{side}', but must be 'left' or 'right'."
+        errmsg = f"Given side: '{side}', but must be 'eyebrow-left' or 'eyebrow-right'."
         alog.error(errmsg)
         raise ValueError(errmsg)
     else:
